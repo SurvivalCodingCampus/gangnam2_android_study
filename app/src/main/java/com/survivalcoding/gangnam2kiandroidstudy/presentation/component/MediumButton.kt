1a5ca18ae5b50d666c7e379e8c3d692be8e5901c
MisterJerry123 <misterjerry12345@gmail.com>
2025-12-03 18:13:27 +0900

    feat: 연습문제1 버튼들 디자인

---
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/component/MediumButton.kt
@@ -0,0 +1,66 @@
+package com.survivalcoding.gangnam2kiandroidstudy.presentation.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowForward
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.survivalcoding.gangnam2kiandroidstudy.ui.AppColors
+import com.survivalcoding.gangnam2kiandroidstudy.ui.AppFonts
+
+@Composable
+fun MediumButton(text: String, onClick: () -> Unit) {
+
+    Box(
+        modifier = Modifier
+            .background(color = AppColors.primary100, shape = RoundedCornerShape(10.dp))
+            .size(width = 243.dp, height = 54.dp)
+            .clickable(onClick = onClick),
+        contentAlignment = Alignment.Center
+    ) {
+        Row {
+            Box(
+                modifier = Modifier
+                    .size(width = 114.dp, height = 24.dp), contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = text,
+                    fontFamily = AppFonts.poppins,
+                    fontWeight = FontWeight.Bold,
+                    color = AppColors.white,
+                    fontSize = 16.sp
+                )
+            }
+            Spacer(modifier = Modifier.size(9.dp))
+            Icon(
+                Icons.AutoMirrored.Filled.ArrowForward,
+                contentDescription = "화살표",
+                tint = AppColors.white
+            )
+
+
+        }
+
+    }
+
+}
+
+@Preview
+@Composable
+fun MediumButtonPreview() {
+    MediumButton(text = "Button", onClick = {})
+
+}
--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/ui/AppFonts.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/ui/AppFonts.kt
@@ -2,12 +2,14 @@ package com.survivalcoding.gangnam2kiandroidstudy.ui
 
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import com.survivalcoding.gangnam2kiandroidstudy.R
 
 object AppFonts {
     val poppins = FontFamily(
-        Font(R.font.poppins_regular),
-        Font(R.font.poppins_bold),
+        Font(R.font.poppins_regular, weight = FontWeight.Normal),
+        Font(R.font.poppins_bold, weight = FontWeight.Bold),
+        Font(R.font.poppins_semibold, weight = FontWeight.SemiBold)
         )
 
 }
