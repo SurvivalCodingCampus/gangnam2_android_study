7bbfad74059d31f31c924a176da7c3e9c8ebb4b9
이영현 <hong.git@yahoo.com>
2025-12-03 20:11:08 +0900

    feat: Butten text 중앙으로

--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/component/BigButton.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/component/BigButton.kt
@@ -1,5 +1,7 @@
 package com.survivalcoding.gangnam2kiandroidstudy.presentation.component
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -9,6 +11,7 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -34,10 +37,17 @@ fun BigButton(
             contentColor = AppColors.white
         )
     ) {
-        Text(
-            text = text,
-            style = AppTextStyles.normalTextBold,
+        Box(
+            modifier = Modifier
+                .size(width = 114.dp, height = 24.dp),
+            contentAlignment = Alignment.Center,
         )
+        {
+            Text(
+                text = text,
+                style = AppTextStyles.normalTextBold,
+            )
+        }
         Spacer(modifier = Modifier.width(11.dp))
         Icon(
             painter = painterResource(R.drawable.arrow_forward_24px),
