ddbd36c8ae6305d8fba03ce54486712834db8004
Junsuk Oh <junsuk.oh@survivalcoding.com>
2025-12-04 12:22:46 +0900

    feat: 컴포넌트들

--- app/src/androidTest/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/component/BigButtonTest.kt
+++ app/src/androidTest/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/component/BigButtonTest.kt
@@ -1,6 +1,5 @@
 package com.survivalcoding.gangnam2kiandroidstudy.presentation.component
 
-import androidx.compose.material.icons.Icons
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
@@ -30,6 +29,7 @@ class BigButtonTest {
         composeTestRule.onNodeWithText("Button").performClick()
 
         assertEquals(1, counter)
+
     }
 
 }
--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/component/BigButton.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/component/BigButton.kt
@@ -7,22 +7,21 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowForward
-import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.survivalcoding.gangnam2kiandroidstudy.R
 import com.survivalcoding.gangnam2kiandroidstudy.ui.AppColors
 import com.survivalcoding.gangnam2kiandroidstudy.ui.AppTextStyles
 
@@ -34,7 +33,7 @@ fun BigButton(
 ) {
     Box(
         modifier = modifier
-            .size(width = 315.dp, height = 60.dp)
+            .height(60.dp)
             .background(
                 color = Color(0xFF129575),
                 shape = RoundedCornerShape(10.dp)
@@ -64,7 +63,8 @@ fun BigButton(
 //                contentDescription = "오른쪽 화살표"
 //            )
             Icon(
-                Icons.AutoMirrored.Filled.ArrowForward,
+                imageVector = Icons.AutoMirrored.Filled.ArrowForward,
+                modifier = Modifier.size(20.dp),
                 tint = AppColors.white,
                 contentDescription = "오른쪽 화살표"
             )
@@ -75,5 +75,14 @@ fun BigButton(
 @Preview(showBackground = true)
 @Composable
 fun BigButtonPreview() {
-    BigButton("Button")
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        BigButton(
+            text = "Button",
+            modifier = Modifier.padding(horizontal = 30.dp),
+        )
+    }
 }
