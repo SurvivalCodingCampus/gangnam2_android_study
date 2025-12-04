3adb51d2e4504c707db2e983f8cdb7d509c36fe4
MisterJerry123 <misterjerry12345@gmail.com>
2025-12-04 23:54:09 +0900

    feat: 연습문제2 RecipeCard 완성

--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/component/RecipeCard.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/component/RecipeCard.kt
@@ -11,26 +11,27 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
 import com.survivalcoding.gangnam2kiandroidstudy.R
 import com.survivalcoding.gangnam2kiandroidstudy.model.Recipes
 import com.survivalcoding.gangnam2kiandroidstudy.ui.AppColors
 import com.survivalcoding.gangnam2kiandroidstudy.ui.AppTextStyles
-import androidx.compose.foundation.layout.padding // padding import 추가
-import androidx.compose.foundation.shape.CircleShape // CircleShape import 추가
-import androidx.compose.ui.graphics.Color // Color import 추가
 
 @Composable
 fun RecipeCard(recipe: Recipes) {
@@ -40,7 +41,12 @@ fun RecipeCard(recipe: Recipes) {
                 .width(30.dp)
                 .fillMaxHeight()
         )
-        Box(modifier = Modifier.weight(1f).fillMaxHeight().clip(RoundedCornerShape(12.dp))) { // Main Box, clip 추가
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight()
+                .clip(RoundedCornerShape(12.dp))
+        ) { // Main Box, clip 추가
             // 배경 이미지
             AsyncImage(
                 model = recipe.image,
@@ -80,9 +86,10 @@ fun RecipeCard(recipe: Recipes) {
 
                             Text(
                                 text = "${recipe.rating}", // 실제 레시피 평점 사용
-                                style = AppTextStyles.smallerTextRegular.copy(color = AppColors.black) // 색상 지정
+                                style = AppTextStyles.smallerTextRegular.copy(fontSize = 8.sp, color = AppColors.black) // 색상 지정
                             )
                             Spacer(modifier = Modifier.width(7.dp))
+
                         }
                     }
                 }
@@ -111,21 +118,32 @@ fun RecipeCard(recipe: Recipes) {
 
             // 오른쪽 하단 텍스트 및 원 추가
             Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.BottomEnd) {
-                Row(modifier = Modifier.padding(bottom = 10.dp)) { // 여기에 bottom padding 추가
+                Row(
+                    modifier = Modifier.padding(bottom = 10.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) { // 여기에 bottom padding 추가
                     Image(painter = painterResource(R.drawable.timer), contentDescription = "알람")
                     Spacer(modifier = Modifier.width(5.dp)) // 이미지와 텍스트 사이 간격
                     Text(
                         text = "30 min", // 예시로 recipe.time을 사용
-                        modifier = Modifier.width(50.dp), // 필요한 경우 너비 조절
+                        modifier = Modifier.width(38.dp), // 필요한 경우 너비 조절
                         style = AppTextStyles.smallerTextRegular.copy(color = AppColors.white) // 스타일 지정
                     )
-                    Spacer(modifier = Modifier.width(5.dp)) // 텍스트와 원 사이 간격
+                    Spacer(modifier = Modifier.width(10.dp)) // 텍스트와 원 사이 간격
                     Box(
                         modifier = Modifier
                             .size(24.dp) // 24dp 크기
                             .clip(CircleShape) // 원형으로 자르기
-                            .background(Color.White) // 흰색 배경
-                    )
+                            .background(Color.White), // 흰색 배경
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Image(
+                            painter = painterResource(R.drawable.inactive),
+                            contentDescription = ""
+                        )
+
+
+                    }
                     Spacer(modifier = Modifier.width(10.dp)) // 여기에 오른쪽 여백을 추가
                 }
             }
