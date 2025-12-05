fe17539abd3b36f537d970c292eb4afffe986baf
이영현 <hong.git@yahoo.com>
2025-12-05 23:28:19 +0900

    feat: icon box 생성

--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/signup/SignUpScreen.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/signup/SignUpScreen.kt
@@ -1,8 +1,10 @@
 package com.survivalcoding.gangnam2kiandroidstudy.presentation.signup
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -18,18 +20,24 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.survivalcoding.gangnam2kiandroidstudy.R
+import com.survivalcoding.gangnam2kiandroidstudy.presentation.component.BigButton
 import com.survivalcoding.gangnam2kiandroidstudy.presentation.component.InputField
+import com.survivalcoding.gangnam2kiandroidstudy.presentation.component.LeeCheckBox
+import com.survivalcoding.gangnam2kiandroidstudy.presentation.component.LeeIconBox
+import com.survivalcoding.gangnam2kiandroidstudy.presentation.component.LeeLine
 import com.survivalcoding.gangnam2kiandroidstudy.ui.AppColors
 import com.survivalcoding.gangnam2kiandroidstudy.ui.AppTextStyles
 
 @Composable
 fun SignUpScreen(
     onSignUpClick: () -> Unit = {},
-    ) {
+) {
 
     var name by remember { mutableStateOf("") }
     var email by remember { mutableStateOf("") }
@@ -43,7 +51,7 @@ fun SignUpScreen(
             .background(MaterialTheme.colorScheme.background)
             .verticalScroll(rememberScrollState())
             .padding(horizontal = 30.dp),
-        ) {
+    ) {
 
         Spacer(modifier = Modifier.height(54.dp))
 
@@ -99,8 +107,49 @@ fun SignUpScreen(
 
         Spacer(modifier = Modifier.height(20.dp))
 
-        Box() {
+        Row() {
+            LeeCheckBox(checked = false, onCheckedChange = {})
+            Spacer(modifier = Modifier.padding(4.dp))
+            Text(
+                "Accept terms & Condition",
+                style = AppTextStyles.smallerTextRegular,
+                color = AppColors.secondary100,
+            )
+        }
 
+        Spacer(modifier = Modifier.height(26.dp))
+
+        BigButton(
+            text = "Sign Up",
+            onClick = onSignUpClick,
+        )
+
+        Spacer(modifier = Modifier.height(14.dp))
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+        ) {
+            LeeLine()
+            Spacer(modifier = Modifier.padding(4.dp))
+            Text(
+                "Or Sign in With",
+                style = AppTextStyles.smallerTextRegular,
+                color = AppColors.secondary100,
+            )
+            Spacer(modifier = Modifier.padding(4.dp))
+            LeeLine()
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+        ) {
+            LeeIconBox(icon = R.drawable.social_icons_google)
+            Spacer(modifier = Modifier.padding(10.dp))
+            LeeIconBox(icon = R.drawable.social_icons_facebook)
         }
 
     }
