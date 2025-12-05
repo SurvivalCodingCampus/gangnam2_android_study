8511ddca525eae057b03755d69285187814bb119
japygo <hyunnjinn@gmail.com>
2025-12-05 17:27:10 +0900

    feat: Add sign-up screen and UI tests
    Adds a new sign-up screen that allows users to create an account. The screen includes input fields for name, email, and password, along with social sign-in options.
    
    - **`SignUpScreen.kt`**:
      - Implements a vertically scrollable sign-up form with fields for Name, Email, Password, and Confirm Password.
      - Utilizes the existing `InputField` and `BigButton` components.
      - Adds a "Terms & Condition" checkbox.
      - Includes "Sign in with Google/Facebook" buttons and a link to the sign-in page for existing users.
      - Configures keyboard actions to move focus between input fields for better usability.
    
    - **`SignUpScreenTest.kt`**:
      - Adds a new UI test file for the `SignUpScreen`.
      - The test verifies that all UI elements, including titles, text fields, and buttons, are correctly displayed.
      - It also tests the text input functionality by entering sample data into the fields and asserting that the new values are present.

---
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/signup/SignUpScreen.kt
@@ -0,0 +1,286 @@
+package com.survivalcoding.gangnam2kiandroidstudy.presentation.signup
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.focusable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.survivalcoding.gangnam2kiandroidstudy.R
+import com.survivalcoding.gangnam2kiandroidstudy.presentation.component.BigButton
+import com.survivalcoding.gangnam2kiandroidstudy.presentation.component.InputField
+import com.survivalcoding.gangnam2kiandroidstudy.ui.AppColors
+import com.survivalcoding.gangnam2kiandroidstudy.ui.AppTextStyles
+
+@Composable
+fun SignUpScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .background(color = AppColors.White)
+            .statusBarsPadding()
+            .navigationBarsPadding()
+            .padding(horizontal = 30.dp),
+    ) {
+        val (name, setName) = rememberSaveable { mutableStateOf("") }
+        val (email, setEmail) = rememberSaveable { mutableStateOf("") }
+        val (password, setPassword) = rememberSaveable { mutableStateOf("") }
+        val (confirmPassword, setConfirmPassword) = rememberSaveable { mutableStateOf("") }
+        val (checked, setChecked) = rememberSaveable { mutableStateOf(false) }
+
+        val focusManager = LocalFocusManager.current
+
+        Column(
+            modifier = Modifier.padding(top = 10.dp, bottom = 20.dp),
+            verticalArrangement = Arrangement.spacedBy(5.dp),
+        ) {
+            Text(
+                text = "Create an account",
+                style = AppTextStyles.PoppinsLargeBold,
+            )
+            Text(
+                text = "Let's help you set up your account,\nit won't take long.",
+                style = AppTextStyles.PoppinsSmallerRegular,
+            )
+        }
+
+        Column(
+            verticalArrangement = Arrangement.spacedBy(20.dp),
+        ) {
+            InputField(
+                label = "Name",
+                value = name,
+                onValueChange = setName,
+                placeholder = "Enter Name",
+                keyboardActions = KeyboardActions(
+                    onNext = {
+                        focusManager.moveFocus(FocusDirection.Down)
+                    },
+                ),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Next,
+                ),
+            )
+            InputField(
+                label = "Email",
+                value = email,
+                onValueChange = setEmail,
+                placeholder = "Enter Email",
+                keyboardActions = KeyboardActions(
+                    onNext = {
+                        focusManager.moveFocus(FocusDirection.Down)
+                    },
+                ),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Email,
+                    imeAction = ImeAction.Next,
+                ),
+            )
+            InputField(
+                label = "Password",
+                value = password,
+                onValueChange = setPassword,
+                placeholder = "Enter Password",
+                visualTransformation = PasswordVisualTransformation(),
+                keyboardActions = KeyboardActions(
+                    onDone = {
+                        focusManager.moveFocus(FocusDirection.Down)
+                    },
+                ),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Password,
+                    imeAction = ImeAction.Next,
+                ),
+            )
+            InputField(
+                label = "Confirm Password",
+                value = confirmPassword,
+                onValueChange = setConfirmPassword,
+                placeholder = "Retype Password",
+                visualTransformation = PasswordVisualTransformation(),
+                keyboardActions = KeyboardActions(
+                    onDone = {
+                        focusManager.moveFocus(FocusDirection.Down)
+                    },
+                ),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Password,
+                    imeAction = ImeAction.Done,
+                ),
+            )
+        }
+
+        Row(
+            modifier = Modifier
+                .padding(top = 20.dp, bottom = 26.dp)
+                .padding(horizontal = 10.dp)
+                .focusable()
+                .clickable {
+                    setChecked(!checked)
+                },
+            horizontalArrangement = Arrangement.spacedBy(5.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Checkbox(
+                modifier = Modifier
+                    .size(17.dp)
+                    .clip(RoundedCornerShape(50.dp)),
+                checked = checked,
+                onCheckedChange = setChecked,
+                colors = CheckboxDefaults.colors().copy(
+                    checkedBorderColor = AppColors.Secondary100,
+                    checkedBoxColor = AppColors.Secondary100,
+                    uncheckedBorderColor = AppColors.Secondary100,
+                ),
+            )
+            Text(
+                text = "Accept terms & Condition",
+                style = AppTextStyles.PoppinsSmallerRegular.copy(color = AppColors.Secondary100),
+            )
+        }
+
+        BigButton(text = "Sign Up") {}
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 60.dp)
+                .padding(top = 14.dp, bottom = 20.dp),
+            horizontalArrangement = Arrangement.spacedBy(7.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            HorizontalDivider(
+                thickness = 1.dp,
+                color = AppColors.Gray4,
+                modifier = Modifier.weight(1f),
+            )
+            Text(
+                text = "Or Sign in With",
+                style = AppTextStyles.PoppinsSmallerRegular.copy(color = AppColors.Gray4),
+            )
+            HorizontalDivider(
+                thickness = 1.dp,
+                color = AppColors.Gray4,
+                modifier = Modifier.weight(1f),
+            )
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 101.dp),
+            horizontalArrangement = Arrangement.spacedBy(25.dp, Alignment.CenterHorizontally),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(44.dp)
+                    .shadow(
+                        elevation = 8.dp,
+                        shape = RoundedCornerShape(10.dp),
+                        ambientColor = Color(0xFF696969),
+                        spotColor = Color(0xFF696969),
+                    )
+                    .background(
+                        color = AppColors.White,
+                        shape = RoundedCornerShape(10.dp),
+                    )
+                    .clickable {}
+                    .padding(10.dp),
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.google),
+                    contentDescription = "google",
+                    modifier = Modifier.size(24.dp),
+                )
+            }
+
+            Box(
+                modifier = Modifier
+                    .size(44.dp)
+                    .shadow(
+                        elevation = 8.dp,
+                        shape = RoundedCornerShape(10.dp),
+                        ambientColor = Color(0xFF696969),
+                        spotColor = Color(0xFF696969),
+                    )
+                    .background(
+                        color = AppColors.White,
+                        shape = RoundedCornerShape(10.dp),
+                    )
+                    .clickable {}
+                    .padding(10.dp),
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.facebook),
+                    contentDescription = "facebook",
+                )
+            }
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 20.dp),
+            horizontalArrangement = Arrangement.spacedBy(2.dp, Alignment.CenterHorizontally),
+        ) {
+            Text(
+                text = "Already a member?",
+                style = AppTextStyles.PoppinsSmallerRegular.copy(
+                    color = AppColors.Black,
+                    fontWeight = FontWeight.SemiBold,
+                ),
+            )
+            Text(
+                text = "Sign In",
+                style = AppTextStyles.PoppinsSmallerRegular.copy(
+                    color = AppColors.Secondary100,
+                    fontWeight = FontWeight.SemiBold,
+                ),
+                modifier = Modifier.clickable {},
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun SignUpScreenPreview() {
+    SignUpScreen()
+}
