29442ae359276f1a5633f766986cfcf4d22320e0
Jieun Lee <geniee1750@gmail.com>
2025-12-09 17:14:18 +0900

    [과제] feat: 연습문제 2 SplashScreen 완성

--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/MainActivity.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/MainActivity.kt
@@ -13,7 +13,7 @@ import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.survivalcoding.gangnam2kiandroidstudy.presentation.sign_in.SignUpScreen
+import com.survivalcoding.gangnam2kiandroidstudy.presentation.screen.splash.SplashScreen
 import com.survivalcoding.gangnam2kiandroidstudy.presentation.util.KeyboardVisibilityHandler
 import com.survivalcoding.gangnam2kiandroidstudy.ui.theme.AppColors
 
@@ -31,6 +31,9 @@ class MainActivity : ComponentActivity() {
                         .fillMaxSize()
                         .windowInsetsPadding(WindowInsets.safeDrawing) // 상단 시스템 바 침범 방지
                 ) {
+                    SplashScreen(
+                        modifier = Modifier.fillMaxSize()
+                    )
 
                     Column(
                         modifier = Modifier
@@ -38,8 +41,9 @@ class MainActivity : ComponentActivity() {
                             .padding(horizontal = 30.dp),
                     ) {
 
+
                         //SignInScreen()
-                        SignUpScreen()
+                        //SignUpScreen()
                         // RecipeScreen()
                     }
                 }
