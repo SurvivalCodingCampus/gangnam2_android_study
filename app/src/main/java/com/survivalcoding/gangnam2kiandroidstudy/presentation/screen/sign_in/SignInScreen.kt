6c6f1b9c1207ce1ec8011cea020efce6f2731bb4
Neouul <fluctu0q0@gmail.com>
2025-12-09 16:47:11 +0900

    chore: screen 디렉토리 추가 후 이동

--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/screen/sign_in/SignInScreen.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/screen/sign_in/SignInScreen.kt
@@ -1,4 +1,4 @@
-package com.survivalcoding.gangnam2kiandroidstudy.presentation.sign_in
+package com.survivalcoding.gangnam2kiandroidstudy.presentation.screen.sign_in
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
