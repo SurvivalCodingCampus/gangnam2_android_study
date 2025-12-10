21f7392d860762dbdc95e716df99d1f5e8b60ae9
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-12-10 22:42:26 +0900

    feat: 코드레빗

--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/search/SearchRecipeState.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/search/SearchRecipeState.kt
@@ -3,7 +3,7 @@ package com.survivalcoding.gangnam2kiandroidstudy.presentation.search
 import com.survivalcoding.gangnam2kiandroidstudy.data.model.Recipe
 
 data class SearchRecipeState(
-    var searchQuery: String = "",
+    val searchQuery: String = "",
     val searchText: String = "Recent Search",
     val recipes: List<Recipe> = emptyList(),
     val filteredRecipes: List<Recipe> = emptyList(),
