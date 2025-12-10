1ad31fbee86601d4fd483db621856747489c0313
MisterJerry123 <misterjerry12345@gmail.com>
2025-12-10 23:41:07 +0900

    feat: 검색 결과 필터링을 위한 상태 추가

--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/screen/search_recipes/SearchRecipesState.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/screen/search_recipes/SearchRecipesState.kt
@@ -5,6 +5,10 @@ import com.survivalcoding.gangnam2kiandroidstudy.model.Recipe
 data class SearchRecipesState(
     val searchInputText: String = "",
     val resultRecipes: List<Recipe> = emptyList(),
+    val selectedTime: String = "",
+    val selectedRate: String = "",
+    val selectedCategory: String = ""
+
 
 
     )
