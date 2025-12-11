d838f7b5446a3629f8858a540da4c04e1df47798
hhp227 <hong227@naver.com>
2025-12-11 14:37:08 +0900

    feat: SavedRecipesRoot 추가, UI 분리

---
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/savedrecipes/SavedRecipesRoot.kt
@@ -0,0 +1,19 @@
+package com.survivalcoding.gangnam2kiandroidstudy.presentation.savedrecipes
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.survivalcoding.gangnam2kiandroidstudy.di.DependencyContainer
+
+@Composable
+fun SavedRecipesRoot(
+    viewModel: SavedRecipesViewModel = viewModel(
+        factory = DependencyContainer.provideSavedRecipesViewModelFactory(LocalContext.current)
+    )
+) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    SavedRecipesScreen(uiState)
+}
