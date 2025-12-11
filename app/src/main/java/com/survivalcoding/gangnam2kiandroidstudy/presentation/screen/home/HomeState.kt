af489ac6a58377e20bc1ac72027d064ca66f9908
MisterJerry123 <misterjerry12345@gmail.com>
2025-12-11 19:05:52 +0900

    feat: 레시피 목록을 표시하기 위한 HomeState 업데이트
    - API로부터 받은 레시피 목록을 저장하기 위해 `resultRecipes` 필드 추가

--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/screen/home/HomeState.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/screen/home/HomeState.kt
@@ -1,5 +1,8 @@
 package com.survivalcoding.gangnam2kiandroidstudy.presentation.screen.home
 
+import com.survivalcoding.gangnam2kiandroidstudy.model.Recipe
+
 data class HomeState(
-    val selectedCategory: String = "All"
+    val selectedCategory: String = "All",
+    val resultRecipes: List<Recipe> = emptyList()
 )
