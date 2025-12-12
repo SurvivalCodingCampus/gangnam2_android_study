89d62b847e2a4bd22c63bcc884148a5a834de603
MisterJerry123 <misterjerry12345@gmail.com>
2025-12-12 14:58:11 +0900

    feat: 네비게이션 기능 추가
    - `NavigationRoot`를 `MainActivity`의 시작점으로 설정

--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/MainActivity.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/MainActivity.kt
@@ -4,35 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.survivalcoding.gangnam2kiandroidstudy.presentation.component.InputField
-import com.survivalcoding.gangnam2kiandroidstudy.ui.theme.Gangnam2kiAndroidStudyTheme
+import com.survivalcoding.gangnam2kiandroidstudy.core.rounting.NavigationRoot
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            InputField("Label", "", "Placeholder", onValueChange = {})
+            NavigationRoot()
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Gangnam2kiAndroidStudyTheme {
-        Greeting("Android")
-    }
-}
