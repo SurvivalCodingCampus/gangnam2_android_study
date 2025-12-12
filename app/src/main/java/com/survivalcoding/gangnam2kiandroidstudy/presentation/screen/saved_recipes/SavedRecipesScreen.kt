41fcff8502b6661240cafb164feeccb92b989740
MisterJerry123 <misterjerry12345@gmail.com>
2025-12-12 17:14:10 +0900

    refactor: SavedRecipesScreen에서 하단 네비게이션 바 제거
    - 불필요한 레이아웃 및 컴포저블을 삭제하여 스크린 구조를 단순화
    - ViewModel 초기화 로직을 Application Context를 사용하도록 수정

--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/screen/saved_recipes/SavedRecipesScreen.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/screen/saved_recipes/SavedRecipesScreen.kt
@@ -1,16 +1,12 @@
 package com.survivalcoding.gangnam2kiandroidstudy.presentation.screen.saved_recipes
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
@@ -19,19 +15,23 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.survivalcoding.gangnam2kiandroidstudy.R
+import com.survivalcoding.gangnam2kiandroidstudy.RecipeAppApplication
 import com.survivalcoding.gangnam2kiandroidstudy.model.Recipe
 import com.survivalcoding.gangnam2kiandroidstudy.presentation.component.RecipeCard
-import com.survivalcoding.gangnam2kiandroidstudy.ui.AppColors
 import com.survivalcoding.gangnam2kiandroidstudy.ui.AppTextStyles
 
 @Composable
-fun SavedRecipesScreen(viewModel: SavedRecipesViewModel = viewModel(factory = SavedRecipesViewModel.Factory)) {
+fun SavedRecipesScreen(
+    viewModel: SavedRecipesViewModel = viewModel(
+        factory = SavedRecipesViewModel.factory(
+            application = LocalContext.current.applicationContext as RecipeAppApplication
+        )
+    )
+) {
     val savedRecipe by viewModel.savedRecipes.collectAsState()
     Box {
         Column(
@@ -55,56 +55,10 @@ fun SavedRecipesScreen(viewModel: SavedRecipesViewModel = viewModel(factory = Sa
                     RecipeCard(
                         recipe = recipe
                     )
-
                 }
             }
-            Spacer(modifier = Modifier.height(92.dp))
         }
     }
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(horizontal = 40.dp),
-        verticalArrangement = Arrangement.Bottom
-    ) {
-        Row(modifier = Modifier.fillMaxWidth()) {
-            Image(
-                painter = painterResource(R.drawable.outline_home),
-                colorFilter = ColorFilter.tint(AppColors.gray4),
-                contentDescription = "Home",
-                modifier = Modifier
-                    .weight(1f)
-            )
-            Image(
-                painter = painterResource(R.drawable.inactive),
-                colorFilter = ColorFilter.tint(AppColors.primary100),
-
-
-                contentDescription = "inactive",
-                modifier = Modifier
-                    .weight(1f)
-                    .size(24.dp)
-            )
-            Image(
-                painter = painterResource(R.drawable.outline_notification_bing),
-                colorFilter = ColorFilter.tint(AppColors.gray4),
-
-                contentDescription = "notification",
-                modifier = Modifier.weight(1f)
-
-            )
-            Image(
-                painter = painterResource(R.drawable.outline_profile),
-                colorFilter = ColorFilter.tint(AppColors.gray4),
-                contentDescription = "profile",
-                modifier = Modifier
-                    .weight(1f)
-            )
-
-        }
-        Spacer(modifier = Modifier.height(58.dp))
-    }
-
 }
 
 @Preview(showBackground = true)
