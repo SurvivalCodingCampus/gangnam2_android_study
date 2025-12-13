3daef44ce6f5c04540bdbcec25fdb0b6ee9624ac
이영현 <hong.git@yahoo.com>
2025-12-14 01:26:10 +0900

    feat: SavedRecipesRoot 로 수정

--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/core/routing/NavigationRoot.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/core/routing/NavigationRoot.kt
@@ -14,7 +14,6 @@ import com.survivalcoding.gangnam2kiandroidstudy.presentation.main.MainScreen
 import com.survivalcoding.gangnam2kiandroidstudy.presentation.notifications.NotificationsScreen
 import com.survivalcoding.gangnam2kiandroidstudy.presentation.profile.ProfileScreen
 import com.survivalcoding.gangnam2kiandroidstudy.presentation.savedrecipes.SavedRecipesRoot
-import com.survivalcoding.gangnam2kiandroidstudy.presentation.savedrecipes.SavedRecipesScreen
 import com.survivalcoding.gangnam2kiandroidstudy.presentation.signin.SignInScreen
 import com.survivalcoding.gangnam2kiandroidstudy.presentation.signup.SignUpScreen
 import com.survivalcoding.gangnam2kiandroidstudy.presentation.splash.SplashScreen
@@ -89,7 +88,7 @@ fun NavigationRoot(
                 HomeRoot()
             }
 
-            entry<Route.SavedRecipes> { SavedRecipesScreen() }
+            entry<Route.SavedRecipes> { SavedRecipesRoot() }
 
             entry<Route.Main> {
                 val backStack = rememberNavBackStack(Route.Home)
