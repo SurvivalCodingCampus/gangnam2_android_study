06d6a6f30ba2ae7c208e7e1520bc139e9fd6feaa
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-12-16 14:55:40 +0900

    feat: 연습문제 1번

--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/savedrecipe/SavedRecipesScreen.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/savedrecipe/SavedRecipesScreen.kt
@@ -1,11 +1,14 @@
 package com.survivalcoding.gangnam2kiandroidstudy.presentation.savedrecipe
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
@@ -15,16 +18,18 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.survivalcoding.gangnam2kiandroidstudy.presentation.component.RecipeCard
+import com.survivalcoding.gangnam2kiandroidstudy.ui.AppColors
 import com.survivalcoding.gangnam2kiandroidstudy.ui.AppTextStyles
 
 @Composable
 fun SavedRecipesScreen(
+    modifier: Modifier = Modifier,
     state: SavedRecipesState= SavedRecipesState(),
     onBookmarkClick: (Int) -> Unit = {},
 ) {
 
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = modifier.fillMaxSize().background(AppColors.white),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Spacer(modifier = Modifier.height(54.dp))
@@ -38,6 +43,9 @@ fun SavedRecipesScreen(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(horizontal = 30.dp),
+            contentPadding = PaddingValues(
+                bottom = 120.dp
+            )
         ) {
             items(state.recipes) { recipe ->
                 RecipeCard(
--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/savedrecipe/SavedRecipesScreenRoot.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/savedrecipe/SavedRecipesScreenRoot.kt
@@ -2,6 +2,7 @@ package com.survivalcoding.gangnam2kiandroidstudy.presentation.savedrecipe
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.MutableCreationExtras
@@ -10,6 +11,7 @@ import com.survivalcoding.gangnam2kiandroidstudy.AppApplication
 
 @Composable
 fun SavedRecipesScreenRoot(
+    modifier: Modifier = Modifier,
     viewModel: SavedRecipesViewModel = viewModel(
         factory = SavedRecipesViewModel.Factory,
         extras = MutableCreationExtras().apply {
@@ -22,6 +24,7 @@ fun SavedRecipesScreenRoot(
 ) {
     val state = viewModel.state.collectAsState()
     SavedRecipesScreen(
+        modifier = modifier,
         state = state.value,
         onBookmarkClick = {
             viewModel.getRecipes(it)
