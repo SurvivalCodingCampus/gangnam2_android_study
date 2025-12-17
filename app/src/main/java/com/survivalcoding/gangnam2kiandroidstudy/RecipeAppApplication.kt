0d5545f24bdd484e074d686ea8d6a70eb446588e
MisterJerry123 <misterjerry12345@gmail.com>
2025-12-17 19:33:46 +0900

    chore: Koin을 사용하도록 Application 클래스 수정
    - 수동으로 생성하던 의존성 코드 제거
    - Koin 모듈을 로드하도록 `onCreate` 메서드 수정

--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/RecipeAppApplication.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/RecipeAppApplication.kt
@@ -6,40 +6,11 @@ import com.survivalcoding.gangnam2kiandroidstudy.core.di.dataSourceModule
 import com.survivalcoding.gangnam2kiandroidstudy.core.di.repositoryModule
 import com.survivalcoding.gangnam2kiandroidstudy.core.di.useCaseModule
 import com.survivalcoding.gangnam2kiandroidstudy.core.di.viewModelModule
-import com.survivalcoding.gangnam2kiandroidstudy.data.data_source.procedure.ProcedureDataSourceImpl
-import com.survivalcoding.gangnam2kiandroidstudy.data.data_source.savedRecipes.SavedRecipesDataSourceImpl
-import com.survivalcoding.gangnam2kiandroidstudy.data.repository.ProcedureRepositoryImpl
-import com.survivalcoding.gangnam2kiandroidstudy.data.repository.SavedRecipesRepositoryImpl
-import com.survivalcoding.gangnam2kiandroidstudy.domain.use_case.GetRecipeDetailsUseCase
-import com.survivalcoding.gangnam2kiandroidstudy.domain.use_case.GetSavedRecipesUseCase
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 
 class RecipeAppApplication : Application() {
-
-
-    val savedRecipesDataSource by lazy { SavedRecipesDataSourceImpl() }
-    val savedRecipesRepository by lazy { SavedRecipesRepositoryImpl(savedRecipesDataSource) }
-    val savedRecipesUseCase by lazy { GetSavedRecipesUseCase(repository = savedRecipesRepository) }
-
-    val savedRecipeDetailsDataSource by lazy { SavedRecipesDataSourceImpl() }
-    val savedRecipeDetailsRepository by lazy {
-        SavedRecipesRepositoryImpl(
-            savedRecipeDetailsDataSource
-        )
-    }
-
-    val procedureDataSource by lazy { ProcedureDataSourceImpl() }
-    val procedureRepository by lazy { ProcedureRepositoryImpl(procedureDataSource) }
-
-
-    val savedRecipeDetailsUseCase by lazy {
-        GetRecipeDetailsUseCase(
-            savedRecipesRepository = savedRecipeDetailsRepository,
-            proceduresRepository = procedureRepository
-        )
-    }
 
     override fun onCreate() {
         super.onCreate()
@@ -52,9 +23,7 @@ class RecipeAppApplication : Application() {
                 viewModelModule,
                 appModule,
                 useCaseModule
-
             )
         }
     }
-
 }
