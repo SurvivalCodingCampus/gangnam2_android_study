ddaa02ec26f19d2f04086f09622c3ea9043a6d2a
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-12-17 15:45:21 +0900

    feat: 연습문제 1

--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/AppApplication.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/AppApplication.kt
@@ -6,60 +6,11 @@ import com.survivalcoding.gangnam2kiandroidstudy.core.di.datasourceModule
 import com.survivalcoding.gangnam2kiandroidstudy.core.di.repositoryModule
 import com.survivalcoding.gangnam2kiandroidstudy.core.di.useCaseModule
 import com.survivalcoding.gangnam2kiandroidstudy.core.di.viewModelModule
-import com.survivalcoding.gangnam2kiandroidstudy.data.datasource.MockRecipeDataSourceImpl
-import com.survivalcoding.gangnam2kiandroidstudy.data.datasource.MockUserDataSourceImpl
-import com.survivalcoding.gangnam2kiandroidstudy.data.datasource.ProcedureDataSoureImpl
-import com.survivalcoding.gangnam2kiandroidstudy.data.repository.BookmarkRepositoryImpl
-import com.survivalcoding.gangnam2kiandroidstudy.data.repository.IngridentRepositoryImpl
-import com.survivalcoding.gangnam2kiandroidstudy.data.repository.ProcedureRepositoryImpl
-import com.survivalcoding.gangnam2kiandroidstudy.domain.repository.RecipeRepository
-import com.survivalcoding.gangnam2kiandroidstudy.data.repository.RecipeRepositoryImpl
-import com.survivalcoding.gangnam2kiandroidstudy.domain.repository.BookmarkRepository
-import com.survivalcoding.gangnam2kiandroidstudy.domain.repository.IngridentRepository
-import com.survivalcoding.gangnam2kiandroidstudy.domain.repository.ProcedureRepository
-import com.survivalcoding.gangnam2kiandroidstudy.domain.usecase.GetRecipeDetailsUseCase
-import com.survivalcoding.gangnam2kiandroidstudy.domain.usecase.GetSavedRecipesUseCase
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 
 class AppApplication : Application() {
-
-    val recipeRepository: RecipeRepository by lazy {
-        RecipeRepositoryImpl(
-            recipeDataSource = MockRecipeDataSourceImpl()
-        )
-    }
-    val bookmarkRepository: BookmarkRepository by lazy {
-        BookmarkRepositoryImpl(
-            userDataSource = MockUserDataSourceImpl()
-        )
-    }
-    val procedureRepository: ProcedureRepository by lazy {
-        ProcedureRepositoryImpl(
-            procedureDataSource = ProcedureDataSoureImpl()
-        )
-    }
-
-    val ingridentRepository: IngridentRepository by lazy {
-        IngridentRepositoryImpl(
-            recipeDataSource = MockRecipeDataSourceImpl()
-        )
-    }
-    val getSavedRecipesUseCase: GetSavedRecipesUseCase by lazy {
-        GetSavedRecipesUseCase(
-            recipeRepository = recipeRepository,
-            bookmarkRepository = bookmarkRepository
-        )
-    }
-
-    val getRecipeDetailsUseCase: GetRecipeDetailsUseCase by lazy {
-        GetRecipeDetailsUseCase(
-            recipeRepository = recipeRepository,
-            procedureRepository = procedureRepository,
-            ingridentRepository = ingridentRepository
-        )
-    }
 
     override fun onCreate() {
         super.onCreate()
