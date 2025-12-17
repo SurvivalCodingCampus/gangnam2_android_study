65d14db2f92c248ac1de3f93b104907001bdb978
MisterJerry123 <misterjerry12345@gmail.com>
2025-12-17 19:20:26 +0900

    feat: Hilt 적용을 위한 Application 클래스 수정
    - `@HiltAndroidApp` 어노테이션 추가
    - 수동으로 생성하던 의존성 코드 제거

--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/RecipeAppApplication.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/RecipeAppApplication.kt
@@ -1,34 +1,7 @@
 package com.survivalcoding.gangnam2kiandroidstudy
 
 import android.app.Application
-import com.survivalcoding.gangnam2kiandroidstudy.data.data_source.procedure.ProcedureDataSourceImpl
-import com.survivalcoding.gangnam2kiandroidstudy.data.data_source.savedRecipes.SavedRecipesDataSourceImpl
-import com.survivalcoding.gangnam2kiandroidstudy.data.repository.ProcedureRepositoryImpl
-import com.survivalcoding.gangnam2kiandroidstudy.data.repository.SavedRecipesRepositoryImpl
-import com.survivalcoding.gangnam2kiandroidstudy.domain.use_case.GetRecipeDetailsUseCase
-import com.survivalcoding.gangnam2kiandroidstudy.domain.use_case.GetSavedRecipesUseCase
+import dagger.hilt.android.HiltAndroidApp
 
-class RecipeAppApplication : Application() {
-    val savedRecipesDataSource by lazy { SavedRecipesDataSourceImpl() }
-    val savedRecipesRepository by lazy { SavedRecipesRepositoryImpl(savedRecipesDataSource) }
-    val savedRecipesUseCase by lazy { GetSavedRecipesUseCase(repository = savedRecipesRepository) }
-
-    val savedRecipeDetailsDataSource by lazy { SavedRecipesDataSourceImpl() }
-    val savedRecipeDetailsRepository by lazy {
-        SavedRecipesRepositoryImpl(
-            savedRecipeDetailsDataSource
-        )
-    }
-
-    val procedureDataSource by lazy { ProcedureDataSourceImpl() }
-    val procedureRepository by lazy { ProcedureRepositoryImpl(procedureDataSource) }
-
-
-    val savedRecipeDetailsUseCase by lazy {
-        GetRecipeDetailsUseCase(
-            savedRecipesRepository = savedRecipeDetailsRepository,
-            proceduresRepository = procedureRepository
-        )
-    }
-
-}
+@HiltAndroidApp
+class RecipeAppApplication : Application()
