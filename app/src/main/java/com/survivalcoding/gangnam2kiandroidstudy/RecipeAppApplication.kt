12400e506a73e7e3c5e88c9268b28985260bc941
MisterJerry123 <misterjerry12345@gmail.com>
2025-12-17 16:57:13 +0900

    feat: Koin을 사용한 의존성 주입 적용
    - Koin 의존성을 추가하고 Application 클래스에서 `startKoin`을 설정했습니다.
    - DataSource, Repository, UseCase, ViewModel에 대한 Koin 모듈을 추가했습니다.
    - 각 ViewModel에서 ViewModelProvider.Factory 컴패니언 객체를 제거하고 Koin을 통해 의존성을 주입받도록 수정했습니다.

--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/RecipeAppApplication.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/RecipeAppApplication.kt
@@ -1,14 +1,24 @@
 package com.survivalcoding.gangnam2kiandroidstudy
 
 import android.app.Application
+import com.survivalcoding.gangnam2kiandroidstudy.core.di.appModule
+import com.survivalcoding.gangnam2kiandroidstudy.core.di.dataSourceModule
+import com.survivalcoding.gangnam2kiandroidstudy.core.di.repositoryModule
+import com.survivalcoding.gangnam2kiandroidstudy.core.di.useCaseModule
+import com.survivalcoding.gangnam2kiandroidstudy.core.di.viewModelModule
 import com.survivalcoding.gangnam2kiandroidstudy.data.data_source.procedure.ProcedureDataSourceImpl
 import com.survivalcoding.gangnam2kiandroidstudy.data.data_source.savedRecipes.SavedRecipesDataSourceImpl
 import com.survivalcoding.gangnam2kiandroidstudy.data.repository.ProcedureRepositoryImpl
 import com.survivalcoding.gangnam2kiandroidstudy.data.repository.SavedRecipesRepositoryImpl
 import com.survivalcoding.gangnam2kiandroidstudy.domain.use_case.GetRecipeDetailsUseCase
 import com.survivalcoding.gangnam2kiandroidstudy.domain.use_case.GetSavedRecipesUseCase
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
 
 class RecipeAppApplication : Application() {
+
+
     val savedRecipesDataSource by lazy { SavedRecipesDataSourceImpl() }
     val savedRecipesRepository by lazy { SavedRecipesRepositoryImpl(savedRecipesDataSource) }
     val savedRecipesUseCase by lazy { GetSavedRecipesUseCase(repository = savedRecipesRepository) }
@@ -29,6 +39,22 @@ class RecipeAppApplication : Application() {
             savedRecipesRepository = savedRecipeDetailsRepository,
             proceduresRepository = procedureRepository
         )
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger()
+            androidContext(this@RecipeAppApplication)
+            modules(
+                dataSourceModule,
+                repositoryModule,
+                viewModelModule,
+                appModule,
+                useCaseModule
+
+            )
+        }
     }
 
 }
