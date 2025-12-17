b230aabbb9f5be2554882285e6fb36b1ea7e2427
이영현 <hong.git@yahoo.com>
2025-12-17 10:10:29 +0900

    feat: RecipeDetails 카드

--- app/src/androidTest/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/savedrecipes/SavedRecipesViewModelTest.kt
+++ app/src/androidTest/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/savedrecipes/SavedRecipesViewModelTest.kt
@@ -1,25 +1,11 @@
 package com.survivalcoding.gangnam2kiandroidstudy.presentation.savedrecipes
 
-import android.content.Context
-import androidx.test.core.app.ApplicationProvider
-import com.survivalcoding.gangnam2kiandroidstudy.data.datasource.RecipeDataSourceImpl
-import com.survivalcoding.gangnam2kiandroidstudy.data.repository.RecipeRepositoryImpl
-import org.junit.Assert
 import org.junit.Test
 
 class SavedRecipesViewModelTest {
     @Test
     fun getUiState() {
-        val viewModel = SavedRecipesViewModel(
-            recipeRepository = RecipeRepositoryImpl(
-                dataSource = RecipeDataSourceImpl(
-                    context = ApplicationProvider.getApplicationContext<Context>()
-                )
-            ),
-        )
 
-        // then
-        Assert.assertTrue(viewModel.uiState.value.recipes.isNotEmpty())
 
     }
 }
--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/recipedetails/RecipeDetailsScreen.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/recipedetails/RecipeDetailsScreen.kt
@@ -10,28 +10,20 @@ import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil3.compose.AsyncImage
 import com.survivalcoding.gangnam2kiandroidstudy.R
+import com.survivalcoding.gangnam2kiandroidstudy.domain.model.Recipe
 import com.survivalcoding.gangnam2kiandroidstudy.presentation.component.RecipeCard
-import com.survivalcoding.gangnam2kiandroidstudy.presentation.component.SmallButton
-import com.survivalcoding.gangnam2kiandroidstudy.presentation.component.Tabs
 import com.survivalcoding.gangnam2kiandroidstudy.ui.AppColors
 import com.survivalcoding.gangnam2kiandroidstudy.ui.AppTextStyles
-import kotlinx.collections.immutable.ImmutableList
 
 @Composable
 fun RecipeDetailsScreen(
@@ -69,6 +61,7 @@ fun RecipeDetailsScreen(
             )
         }
 
+
         Column(
             modifier = Modifier.fillMaxWidth(),
             verticalArrangement = Arrangement.spacedBy(10.dp),
@@ -78,131 +71,28 @@ fun RecipeDetailsScreen(
                     recipe = it,
                 )
             }
-        }
 
-        Row(
-            modifier = Modifier.padding(horizontal = 5.dp),
-        ) {
-            Text(
-                text = uiState.recipe?.name ?: "",
-                style = AppTextStyles.smallTextBold,
-                maxLines = 2,
-                overflow = TextOverflow.Ellipsis,
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(end = 18.dp),
-            )
-
-            Text(
-                text = "(13K Reviews)",
-                style = AppTextStyles.smallTextBold.copy(AppColors.gray3),
-                maxLines = 2,
-                overflow = TextOverflow.Ellipsis,
-            )
-        }
-
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 10.dp, end = 8.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-        ) {
             Row(
-                horizontalArrangement = Arrangement.spacedBy(10.dp),
-                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(horizontal = 5.dp),
             ) {
-                AsyncImage(
-                    model = uiState.profile?.image,
-                    contentDescription = "profile image",
-                    contentScale = ContentScale.Crop,
+                Text(
+                    text = uiState.recipe?.name ?: "",
+                    style = AppTextStyles.smallTextBold,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis,
                     modifier = Modifier
-                        .size(40.dp)
-                        .clip(CircleShape),
+                        .weight(1f)
+                        .padding(end = 18.dp),
                 )
 
-                Column(
-                    verticalArrangement = Arrangement.spacedBy(2.dp),
-                ) {
-                    Text(
-                        text = uiState.profile?.name ?: "",
-                        style = AppTextStyles.smallTextBold,
-                        maxLines = 1,
-                        overflow = TextOverflow.Ellipsis,
-                    )
-
-                    Row(
-                        horizontalArrangement = Arrangement.spacedBy(1.dp),
-                    ) {
-                        Icon(
-                            painter = painterResource(R.drawable.bold_location),
-                            contentDescription = "location icon",
-                            tint = AppColors.primary80,
-                            modifier = Modifier.size(17.dp),
-                        )
-                        Text(
-                            text = uiState.profile?.address ?: "",
-                            style = AppTextStyles.smallTextBold.copy(AppColors.gray3),
-                            maxLines = 1,
-                            overflow = TextOverflow.Ellipsis,
-                        )
-                    }
-                }
-            }
-
-            SmallButton(
-                text = "Follow",
-                modifier = Modifier.width(100.dp),
-            ) {}
-        }
-
-
-        Tabs(
-            labels = listOf("Ingredient", "Procedure") as ImmutableList<String>,
-            selectedIndex = uiState.selectedTabIndex,
-            onTabSelected = onTabClick,
-        )
-
-
-
-
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 22.dp, bottom = 20.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-        ) {
-            Row(
-                horizontalArrangement = Arrangement.spacedBy(5.dp),
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
-                Icon(
-                    painter = painterResource(R.drawable.ic_timer_outline),
-                    contentDescription = "timer icon",
-                    tint = AppColors.gray3,
-                    modifier = Modifier.size(17.dp),
-                )
                 Text(
-                    text = "1 serve",
-                    style = AppTextStyles.smallTextRegular.copy(AppColors.gray3),
-                )
-            }
-
-            if (uiState.selectedTabIndex == 0) {
-                Text(
-                    text = "${uiState.ingredients.size} items",
-                    style = AppTextStyles.smallerTextRegular.copy(AppColors.gray3),
-                )
-            }
-
-            if (uiState.selectedTabIndex == 1) {
-                Text(
-                    text = "${uiState.procedures.size} steps",
-                    style = AppTextStyles.smallTextRegular.copy(AppColors.gray3),
+                    text = "(${uiState.reviewCount} Reviews)",
+                    style = AppTextStyles.smallTextBold.copy(AppColors.gray3),
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis,
                 )
             }
         }
-
-
 
 
 
@@ -214,6 +104,17 @@ fun RecipeDetailsScreen(
 @Composable
 fun RecipeDetailsScreenPreview() {
     RecipeDetailsScreen(
-
+        uiState = RecipeDetailsState(
+            recipe = Recipe(
+                id = 1,
+                name = "Kimchi Fried Rice",
+                image = "https://cdn.pixabay.com/photo/2017/11/10/15/04/steak-2936531_1280.jpg",
+                chef = "Chef John",
+                time = "20 min",
+                rating = 4.0,
+                category = "Korean",
+            ),
+            reviewCount = 1,
+        )
     )
 }
