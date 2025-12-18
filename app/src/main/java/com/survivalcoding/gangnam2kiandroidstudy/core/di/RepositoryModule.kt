7b83149ae32700ae2a38ea650707d27170fe215d
이영현 <hong.git@yahoo.com>
2025-12-18 10:47:14 +0900

    feat: jakarta.inject.Singleton 종속성 제거

--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/core/di/RepositoryModule.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/core/di/RepositoryModule.kt
@@ -18,7 +18,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import jakarta.inject.Singleton
+import javax.inject.Singleton
 
 
 @Module
