20a06a49861d0103bce4e07e17fb1a75c615d672
MisterJerry123 <misterjerry12345@gmail.com>
2025-12-19 16:44:22 +0900

    feat: SearchRecipesViewModel 이벤트 처리 및 필터 액션 세분화
    - `SearchRecipesEvent`를 통한 스낵바 메시지 전송 로직 추가
    - 필터 적용(`applyFilterRecipe`) 및 취소(`dismissFilterRecipe`) 메서드 추가
    - `SearchRecipesAction`을 `ApplyFilterRecipes`, `CancelFilterRecipes`, `ChangeSearch`로 세분화
    - 불필요한 로그 메시지 제거

--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/screen/search_recipes/SearchRecipesAction.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/screen/search_recipes/SearchRecipesAction.kt
@@ -2,12 +2,28 @@ package com.survivalcoding.gangnam2kiandroidstudy.presentation.screen.search_rec
 
 sealed interface SearchRecipesAction {
     data object ToggleBottomSheet : SearchRecipesAction
-    data class FilterRecipes(
+    data class CancelFilterRecipes(
         val searchText: String,
         val time: String,
         val rate: String,
         val category: String,
         val enableBottomSheet: Boolean
     ) : SearchRecipesAction
+
+    data class ApplyFilterRecipes(
+        val searchText: String,
+        val time: String,
+        val rate: String,
+        val category: String,
+        val enableBottomSheet: Boolean
+    ) : SearchRecipesAction
+
+    data class ChangeSearch(
+        val searchText: String,
+        val time: String,
+        val rate: String,
+        val category: String,
+        val enableBottomSheet: Boolean
+    ): SearchRecipesAction
 
 }
