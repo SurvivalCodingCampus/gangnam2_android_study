3c45e5e4b8ecf29a4626a6301bdff9d3aac8bb0b
MisterJerry123 <misterjerry12345@gmail.com>
2025-12-22 19:28:28 +0900

    feat: SplashViewModel 리팩터링 및 네트워크 감지 로직 변경
    - NetworkObserver를 NetworkMonitor로 교체
    - UI 상태 변수명을 `value`에서 `state`로 변경
    - 네트워크 연결 상태 이벤트를 위한 `event` (SharedFlow) 추가
    - 네트워크 연결 상태에 따라 상태 업데이트 및 이벤트 방출 로직 수정

--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/screen/splash/SplashViewModel.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/screen/splash/SplashViewModel.kt
@@ -2,39 +2,29 @@ package com.survivalcoding.gangnam2kiandroidstudy.presentation.screen.splash
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.survivalcoding.gangnam2kiandroidstudy.core.util.NetworkObserver
+import com.survivalcoding.gangnam2kiandroidstudy.domain.network.NetworkMonitor
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 class SplashViewModel(
-    private val networkObserver: NetworkObserver
+    private val networkMonitor: NetworkMonitor
 ) : ViewModel() {
 
-    private var _value = MutableStateFlow(SplashState())
-    val value = _value.asStateFlow()
+    private var _state = MutableStateFlow(SplashState())
+    val state = _state.asStateFlow()
+
+    private var _event = MutableSharedFlow<Boolean>()
+    val event = _event.asSharedFlow()
+
 
     init {
-        observeNetwork()
-    }
-
-
-    val isNetworkConnected = networkObserver.networkStatus.stateIn(
-        viewModelScope,
-        SharingStarted.WhileSubscribed(5000),
-        false
-    )
-
-    private fun observeNetwork() {
         viewModelScope.launch {
-            networkObserver.networkStatus.collect { isConnected ->
-                // 네트워크 연결 상태에 따라 SplashState의 버튼 활성화 여부를 업데이트
-                _value.update { currentState ->
-                    currentState.copy(isNextButtonEnable = isConnected)
-                }
+            networkMonitor.isConnectedNetwork.collect {
+                _state.value = _state.value.copy(isNextButtonEnable = it)
+                _event.emit(it)
             }
         }
     }
