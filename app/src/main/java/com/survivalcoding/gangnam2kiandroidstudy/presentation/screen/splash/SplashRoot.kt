43c20c0c9b0899f872c1f698ef9931feedbcc4db
MisterJerry123 <misterjerry12345@gmail.com>
2025-12-22 19:28:40 +0900

    feat: SplashRoot에서 Koin ViewModel 사용 및 이벤트 처리 방식 변경
    - `koinViewModel()`을 통한 ViewModel 주입 추가
    - UI 상태(`state`) 및 이벤트(`event`) 관찰 로직 수정
    - 네트워크 미연결 시 스낵바 표시 로직을 이벤트 기반으로 변경

--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/screen/splash/SplashRoot.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/screen/splash/SplashRoot.kt
@@ -1,5 +1,6 @@
 package com.survivalcoding.gangnam2kiandroidstudy.presentation.screen.splash
 
+import android.util.Log
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
@@ -9,13 +10,14 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
+import org.koin.androidx.compose.koinViewModel
 
 
 @Composable
-fun SplashRoot(onStartButtonClick: () -> Unit, viewModel: SplashViewModel) {
-    val state by viewModel.value.collectAsState()
-
+fun SplashRoot(onStartButtonClick: () -> Unit, viewModel: SplashViewModel = koinViewModel()) {
+    val state by viewModel.state.collectAsState()
     val snackbarHostState = remember { SnackbarHostState() }
+
     Scaffold(
         snackbarHost = { SnackbarHost(snackbarHostState) }
     ) {
@@ -26,14 +28,15 @@ fun SplashRoot(onStartButtonClick: () -> Unit, viewModel: SplashViewModel) {
             isEnable = state.isNextButtonEnable
         )
     }
-
-    LaunchedEffect(state.isNextButtonEnable) {
-        if (!state.isNextButtonEnable) {
-            snackbarHostState.showSnackbar("네트워크가 연결 안됨.")
+    LaunchedEffect(Unit) {
+        viewModel.event.collect {
+            Log.d("SplashRoot", "SplashRoot:$it")
+            if (!it) {
+                snackbarHostState.showSnackbar("네트워크가 연결 안됨.")
+            }
         }
 
     }
-
 
 }
 
