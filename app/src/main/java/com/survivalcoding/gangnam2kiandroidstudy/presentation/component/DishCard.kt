a54d1c3f3a7c607f78d17d6debf56e8bae116bc1
Neouul <fluctu0q0@gmail.com>
2025-12-24 18:35:00 +0900

    test: 통합테스트 연습문제 3 - HomeScreen 북마크 테스트 추가

--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/component/DishCard.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/component/DishCard.kt
@@ -26,6 +26,9 @@ import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.stateDescription
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
@@ -103,7 +106,11 @@ fun DishCard(
                         color = AppColors.white,
                         shape = CircleShape,
                     )
-                    .clickable { onBookmarkClick() },
+                    .clickable { onBookmarkClick() }
+                    .semantics {
+                        stateDescription = if (isSaved) "Saved" else "Not Saved"
+                        contentDescription = "Bookmark Button"
+                    },
                 contentAlignment = Alignment.Center,
             ) {
                 Icon(
