3c4cddfd8411b3331901901af0868e6cc1e77ede
MisterJerry123 <misterjerry12345@gmail.com>
2025-12-26 00:39:27 +0900

    feat: 링크 복사 기능 추가

--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/core/di/UseCaseModule.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/core/di/UseCaseModule.kt
@@ -1,5 +1,6 @@
 package com.survivalcoding.gangnam2kiandroidstudy.core.di
 
+import com.survivalcoding.gangnam2kiandroidstudy.domain.use_case.CopyLinkUseCase
 import com.survivalcoding.gangnam2kiandroidstudy.domain.use_case.DeleteSavedRecipeUseCase
 import com.survivalcoding.gangnam2kiandroidstudy.domain.use_case.GetRecipeDetailsUseCase
 import com.survivalcoding.gangnam2kiandroidstudy.domain.use_case.GetSavedRecipesUseCase
@@ -9,4 +10,5 @@ val useCaseModule = module {
     single<GetRecipeDetailsUseCase> { GetRecipeDetailsUseCase(get(), get()) }
     single<GetSavedRecipesUseCase> { GetSavedRecipesUseCase(get()) }
     single<DeleteSavedRecipeUseCase> { DeleteSavedRecipeUseCase(get()) }
+    single<CopyLinkUseCase> { CopyLinkUseCase(get()) }
 }
