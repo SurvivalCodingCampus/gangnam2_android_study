54c1fbde216733c937d7264d389e961445133c4e
MisterJerry123 <misterjerry12345@gmail.com>
2025-12-26 01:16:11 +0900

    refactor: RecipesRepositoryImpl 변수명 변경

--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/data/repository/RecipesRepositoryImpl.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/data/repository/RecipesRepositoryImpl.kt
@@ -5,17 +5,17 @@ import com.survivalcoding.gangnam2kiandroidstudy.domain.model.Recipe
 import com.survivalcoding.gangnam2kiandroidstudy.domain.repository.RecipesRepository
 
 class RecipesRepositoryImpl(
-    private val savedRecipesDataSource: RecipesDataSource
+    private val recipesDataSource: RecipesDataSource
 ) : RecipesRepository {
     override suspend fun getAllRecipes(): List<Recipe> {
-        return savedRecipesDataSource.getAllRecipes().recipes
+        return recipesDataSource.getAllRecipes().recipes
     }
 
     override suspend fun getSavedRecipes(): List<Recipe> {
-        return savedRecipesDataSource.getAllRecipes().recipes.filter { it.isSaved }
+        return recipesDataSource.getAllRecipes().recipes.filter { it.isSaved }
     }
 
     override suspend fun deleteSavedRecipe(id: Int) {
-        savedRecipesDataSource.deleteSavedRecipe(id)
+        recipesDataSource.deleteSavedRecipe(id)
     }
 }
