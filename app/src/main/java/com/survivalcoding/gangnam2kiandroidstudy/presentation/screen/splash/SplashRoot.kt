c23cd234717f32c961e287e1bda09676355c889f
MisterJerry123 <misterjerry12345@gmail.com>
2025-12-31 10:17:14 +0900

    refactor: SplashRoot에 Scaffold padding 적용 및 Box 추가
    - Scaffold의 content padding을 적용하기 위해 Box 컨테이너 추가
    - 불필요한 공백 제거

--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/screen/splash/SplashRoot.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/screen/splash/SplashRoot.kt
@@ -1,6 +1,8 @@
 package com.survivalcoding.gangnam2kiandroidstudy.presentation.screen.splash
 
 import android.util.Log
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
@@ -9,6 +11,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import org.koin.androidx.compose.koinViewModel
 
@@ -20,13 +23,15 @@ fun SplashRoot(onStartButtonClick: () -> Unit, viewModel: SplashViewModel = koin
 
     Scaffold(
         snackbarHost = { SnackbarHost(snackbarHostState) }
-    ) {
-        SplashScreen(
-            onStartButtonClick = {
-                onStartButtonClick()
-            },
-            isEnable = state.isNextButtonEnable
-        )
+    ) { it ->
+        Box(modifier = Modifier.padding(it)) {
+            SplashScreen(
+                onStartButtonClick = {
+                    onStartButtonClick()
+                },
+                isEnable = state.isNextButtonEnable
+            )
+        }
     }
     LaunchedEffect(Unit) {
         viewModel.event.collect {
@@ -35,7 +40,6 @@ fun SplashRoot(onStartButtonClick: () -> Unit, viewModel: SplashViewModel = koin
                 snackbarHostState.showSnackbar("네트워크가 연결 안됨.")
             }
         }
-
     }
 
 }
