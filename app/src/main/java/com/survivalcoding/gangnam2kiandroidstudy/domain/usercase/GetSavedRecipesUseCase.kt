3bb6117a75fe621d0c0b386707f3d9bac6dba74e
이영현 <hong.git@yahoo.com>
2026-01-02 02:59:59 +0900

    feat: Recipe 모델에 isBookmarked 필드 추가 및 북마크 기능 연동

--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/domain/usercase/GetSavedRecipesUseCase.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/domain/usercase/GetSavedRecipesUseCase.kt
@@ -1,12 +1,22 @@
 package com.survivalcoding.gangnam2kiandroidstudy.domain.usercase
 
 import com.survivalcoding.gangnam2kiandroidstudy.domain.model.Recipe
+import com.survivalcoding.gangnam2kiandroidstudy.domain.repository.BookmarkRepository
 import com.survivalcoding.gangnam2kiandroidstudy.domain.repository.RecipeRepository
 
 class GetSavedRecipesUseCase(
-    private val recipeRepository: RecipeRepository
+    private val recipeRepository: RecipeRepository,
+    private val bookmarkRepository: BookmarkRepository
 ) {
     suspend operator fun invoke(): List<Recipe> {
-        return recipeRepository.getRecipes()
+        val savedRecipes = recipeRepository.getRecipes()
+
+        bookmarkRepository.getBookmarkedRecipeIds().collect { bookmarkedRecipeIds ->
+            savedRecipes.forEach { recipe ->
+                recipe.isBookmarked = bookmarkedRecipeIds.contains(recipe.id)
+            }
+        }
+
+        return savedRecipes
     }
 }
