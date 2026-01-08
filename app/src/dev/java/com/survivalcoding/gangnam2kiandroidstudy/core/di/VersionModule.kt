502520a808810170c8c1c5851786d18452beb1d8
japygo <hyunnjinn@gmail.com>
2026-01-08 14:33:16 +0900

    feat: BookmarkContentProvider로 북마크 데이터 공유 기능 추가
    - ContentProvider를 통해 북마크 데이터를 외부 애플리케이션에 제공
    - UriMatcher를 사용하여 북마크 데이터 쿼리 처리 로직 구현
    - Koin DI로 BookmarkDao 및 RecipeRepository 주입 설정
    - AndroidManifest에 BookmarkContentProvider 등록
    - Room 데이터베이스 설정 및 의존성 추가

--- app/src/dev/java/com/survivalcoding/gangnam2kiandroidstudy/core/di/VersionModule.kt
+++ app/src/dev/java/com/survivalcoding/gangnam2kiandroidstudy/core/di/VersionModule.kt
@@ -1,8 +1,10 @@
 package com.survivalcoding.gangnam2kiandroidstudy.core.di
 
+import androidx.room.Room
 import com.survivalcoding.gangnam2kiandroidstudy.core.util.NetworkMonitor
 import com.survivalcoding.gangnam2kiandroidstudy.core.util.RealNetworkMonitor
-import com.survivalcoding.gangnam2kiandroidstudy.data.repository.MockBookmarkRepositoryImpl
+import com.survivalcoding.gangnam2kiandroidstudy.data.database.AppDatabase
+import com.survivalcoding.gangnam2kiandroidstudy.data.repository.RoomBookmarkRepositoryImpl
 import com.survivalcoding.gangnam2kiandroidstudy.domain.repository.BookmarkRepository
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
@@ -10,5 +12,13 @@ import org.koin.dsl.module
 val versionModule = module {
     single<NetworkMonitor> { RealNetworkMonitor(androidContext()) }
 
-    single<BookmarkRepository> { MockBookmarkRepositoryImpl }
+    single<AppDatabase> {
+        Room.databaseBuilder(
+            androidContext(),
+            AppDatabase::class.java,
+            "recipe-db",
+        ).build()
+    }
+    single { get<AppDatabase>().bookmarkDao() }
+    single<BookmarkRepository> { RoomBookmarkRepositoryImpl(get()) }
 }
