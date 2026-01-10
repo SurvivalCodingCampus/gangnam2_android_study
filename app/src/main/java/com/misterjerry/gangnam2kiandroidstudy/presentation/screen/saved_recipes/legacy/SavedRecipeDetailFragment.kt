d949ab53cbcc5f41c587ec3188ea4719cac4c93f
MisterJerry123 <misterjerry12345@gmail.com>
2026-01-10 19:30:00 +0900

    feat: SavedRecipeDetailFragment 및 레이아웃 추가
    - 저장된 레시피 상세 정보를 표시하는 SavedRecipeDetailFragment 구현
    - 프래그먼트에서 사용할 fragment_saved_recipe_detail.xml 레이아웃 추가
    - newInstance를 통한 레시피 이름 데이터 전달 로직 포함

---
+++ app/src/main/java/com/misterjerry/gangnam2kiandroidstudy/presentation/screen/saved_recipes/legacy/SavedRecipeDetailFragment.kt
@@ -0,0 +1,52 @@
+package com.misterjerry.gangnam2kiandroidstudy.presentation.screen.saved_recipes.legacy
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.misterjerry.gangnam2kiandroidstudy.databinding.FragmentSavedRecipeDetailBinding
+
+class SavedRecipeDetailFragment() : Fragment() {
+
+    private var _binding: FragmentSavedRecipeDetailBinding? = null
+    private val binding get() = _binding!!
+    private var recipeName: String? = ""
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSavedRecipeDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            recipeName = it.getString(ARG_RECIPE_NAME)
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tvRecipeName.text = recipeName
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+    companion object {
+        private const val ARG_RECIPE_NAME = "recipe_name"
+        fun newInstance(recipeName: String): SavedRecipeDetailFragment {
+            val fragment = SavedRecipeDetailFragment()
+            val args = Bundle()
+            args.putString(ARG_RECIPE_NAME, recipeName)
+            fragment.arguments = args
+            return fragment
+        }
+    }
+}
+
