db536ad282b83203f5c05d1dbd1899a5e12f9aca
MisterJerry123 <misterjerry12345@gmail.com>
2026-01-10 20:44:58 +0900

    feat: RecipeListAdapter 아이템 삭제 기능 및 DiffUtil 적용
    - RecipeListAdapter.OnItemClickListener에 deleteItem 메서드 추가
    - 북마크 버튼 클릭 시 아이템 삭제 요청 로직 구현
    - 효율적인 리스트 업데이트를 위한 DiffUtilCallback 클래스 추가 및 적용
    - 어댑터 내부에 가변 리스트(itemList) 관리 및 updateList 메서드 구현

--- app/src/main/java/com/misterjerry/gangnam2kiandroidstudy/presentation/screen/saved_recipes/legacy/RecipeListAdapter.kt
+++ app/src/main/java/com/misterjerry/gangnam2kiandroidstudy/presentation/screen/saved_recipes/legacy/RecipeListAdapter.kt
@@ -4,6 +4,7 @@ import android.content.res.ColorStateList
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.core.graphics.toColorInt
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import coil3.load
 import com.misterjerry.gangnam2kiandroidstudy.R
@@ -11,12 +12,15 @@ import com.misterjerry.gangnam2kiandroidstudy.databinding.ItemSavedRecipeBinding
 import com.misterjerry.gangnam2kiandroidstudy.domain.model.Recipe
 
 class RecipeListAdapter(
-    private val recipeList: List<Recipe>
+    private val recipeList: List<Recipe>,
 ) : RecyclerView.Adapter<RecipeListAdapter.ViewHolder>() {
 
     interface OnItemClickListener {
         fun onItemClicked(pos: Int)
+        fun deleteItem(recipeId: Int)
     }
+
+    private val itemList = recipeList.toMutableList()
 
     //클릭 리스너 정의
     private var onItemClickListener: OnItemClickListener? = null
@@ -46,13 +50,15 @@ class RecipeListAdapter(
                 if (pos != RecyclerView.NO_POSITION) {
                     onItemClickListener?.onItemClicked(pos)
                 }
-
+            }
+            binding.btnBookmark.setOnClickListener {
+                val pos = getAbsoluteAdapterPosition() // 현재 아이템의 위치
+                if (pos != RecyclerView.NO_POSITION) {
+                    onItemClickListener?.deleteItem(itemList[pos].id)
+                }
             }
         }
-
-
     }
-
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
@@ -67,8 +73,16 @@ class RecipeListAdapter(
         holder: ViewHolder,
         position: Int
     ) {
-        holder.bind(recipeList[position])
+        holder.bind(itemList[position])
     }
 
-    override fun getItemCount(): Int = recipeList.size
+    override fun getItemCount(): Int = itemList.size
+
+    fun updateList(newList: List<Recipe>) {
+        val diffCallback = DiffUtilCallback(itemList, newList)
+        val diffResult = DiffUtil.calculateDiff(diffCallback)
+        itemList.clear()
+        itemList.addAll(newList)
+        diffResult.dispatchUpdatesTo(this)
+    }
 }
