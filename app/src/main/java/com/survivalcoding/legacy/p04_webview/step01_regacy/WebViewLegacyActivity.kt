6e9e54223ce2bc004cb62e4724eeb2a9ea794e22
Junsuk Oh <junsuk.oh@survivalcoding.com>
2026-01-27 12:02:25 +0900

    feat: WebView를 활용한 레거시 웹 뷰 구현 및 JavaScript 인터페이스 연동
    - WebViewLegacyActivity 추가 및 기본 WebView 설정 (JavaScript 활성화, WebViewClient 설정)
    - WebAppInterface 클래스를 통한 Android와 JavaScript 간 Toast 출력 연동 기능 구현
    - 로컬 에셋(index.html, address.html) 및 레이아웃(activity_web_view_legacy.xml) 추가
    - AndroidManifest.xml에 WebViewLegacyActivity 등록 및 런처 액티비티 설정 수정

---
+++ app/src/main/java/com/survivalcoding/legacy/p04_webview/step01_regacy/WebViewLegacyActivity.kt
@@ -0,0 +1,40 @@
+package com.survivalcoding.legacy.p04_webview.step01_regacy
+
+import android.content.Context
+import android.os.Bundle
+import android.webkit.JavascriptInterface
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.survivalcoding.gangnam2kiandroidstudy.R
+
+class WebViewLegacyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_web_view_legacy)
+
+        val webView = findViewById<WebView>(R.id.my_web_view)
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = WebViewClient()
+//        webView.webChromeClient = WebChromeClient()
+        webView.addJavascriptInterface(WebAppInterface(this), "Android")
+
+//        webView.loadUrl("https://naver.com")
+        webView.loadUrl("file:///android_asset/index.html")
+//        webView.loadUrl("file:///android_asset/address.html")
+    }
+}
+
+class WebAppInterface(private val mContext: Context) {
+
+    /** Show a toast from the web page.  */
+    @JavascriptInterface
+    fun showToast(toast: String) {
+        Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show()
+    }
+}
